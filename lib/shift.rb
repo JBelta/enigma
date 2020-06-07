e817d8d4c1ea15ddc33d34c558c130ab4f062be5
James Belta <james.belta@gmail.com>
2020-06-07 08:34:33 -0600

    Build key_pairs method

--- lib/shift.rb
+++ lib/shift.rb
@@ -8,4 +8,12 @@ class Shift
     @keys = Key.new.first_five
     @offsets = Offset.new.last_four
   end
-end 
+
+  def key_pairs
+    key_collection = []
+    @keys.each_cons(2) do |string_1, string_2|
+      key_collection << (string_1+string_2)
+    end
+    key_collection
+  end
+end
