a337c4cf0874c030459a70c321673cf049ac1103
James Belta <james.belta@gmail.com>
2020-06-06 22:50:25 -0600

    Build instance test

---
+++ test/shift_test.rb
@@ -0,0 +1,13 @@
+require './lib/offset'
+require './lib/key'
+require './lib/shift'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class ShiftTest < Minitest::Test
+  def test_it_exists
+    shift = Shift.new
+    assert_instance_of Shift, shift
+  end
+end 
