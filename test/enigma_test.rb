07669153f64dde322be4a0f513ec2b821cdfea44
James Belta <james.belta@gmail.com>
2020-06-07 11:45:48 -0600

    Build enigma test, initialize and attributes

---
+++ test/enigma_test.rb
@@ -0,0 +1,20 @@
+require './lib/enigma'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class EnigmaTest < Minitest::Test
+  def test_it_exists
+    enigma = Enigma.new("message", Shift.new.keys, Shift.new.offsets)
+    assert_instance_of Enigma, enigma
+  end
+
+  def test_it_has_attributes
+    enigma = Enigma.new("message", Shift.new.keys, Shift.new.offsets)
+    assert_instance_of Array, enigma.date
+    assert_equal 4, enigma.date.count
+    assert_instance_of Array, enigma.key
+    assert_equal 5, enigma.key.count
+    assert_instance_of String, enigma.message
+  end
+end
