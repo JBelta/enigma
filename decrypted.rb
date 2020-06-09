7983ee4939d611aa8c63bdb6592ea6004d78c502
James Belta <james.belta@gmail.com>
2020-06-09 03:59:16 -0600

    Build decrypted runner

--- decrypted.rb
+++ decrypted.rb
@@ -3,11 +3,11 @@ require "./lib/enigma"
 enigma = Enigma.new
 
 message = File.open(ARGV[0], "r")
-input = message.read.chomp
-decrypted = enigma.encrypt(input, "02715", "040895")
-
+#require "pry"; binding.pry
+input = message.read
+decrypted = enigma.decrypt(input, ARGV[2], ARGV[3])
 writer = File.open(ARGV[1], "w")
 writer.write(decrypted[:decryption])
 writer.close
 
-puts "Created 'decrypted.txt' with the key #{encrypted[:key]} and the date #{encrypted[:date]}"
+puts "Created 'decrypted.txt' with the key #{decrypted[:key]} and the date #{decrypted[:date]}"
