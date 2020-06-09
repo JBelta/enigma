eef62e0e439f31cfd892577309b2c06a56df4cd2
James Belta <james.belta@gmail.com>
2020-06-08 18:35:06 -0600

    Rebuild test/ rebuilding class

--- test/key_test.rb
+++ test/key_test.rb
@@ -7,18 +7,13 @@ class KeyTest < Minitest::Test
 
   def test_it_exists
     key = Key.new
+    require 'pry'; binding.pry
     assert_instance_of Key, key
   end
 
   def test_it_has_attributes
   key = Key.new
-  assert_equal String, key.numbers.class
-  end
-
-  def test_first_five
-    key = Key.new
-    keys = key.first_five
-    assert_equal Array, keys.class
-    assert_equal 5, keys.count
+  assert_equal Array, key.numbers.class
+  assert_equal 5, key.numbers.count
   end
 end
