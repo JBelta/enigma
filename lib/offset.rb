a1c03a32ce0ba2c8f15b584db5b09ebf7b5b43c6
James Belta <james.belta@gmail.com>
2020-06-08 18:45:44 -0600

    Rebuild class

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,10 +1,11 @@
 require 'date'
 
 class Offset
-  attr_reader :date
+  attr_reader :date, :numbers
 
   def initialize
     @date = Time.now.strftime("%m%d%y")
+    @numbers = (@date.to_i ** 2).to_s.split("").last(4)
   end
 
   def last_four
