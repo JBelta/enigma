d65247821124cab4ea6040461b97db4541905bf3
James Belta <james.belta@gmail.com>
2020-06-08 19:01:46 -0600

    Rebuild test/ rebuilding class

--- test/shiftable_test.rb
+++ test/shiftable_test.rb
@@ -7,14 +7,20 @@ class ShiftableTest < Minitest::Test
   include Shiftable
 
   def test_key_pairs_work_on_strings
-    key = "02715"
-    assert_equal ["02", "27", "71", "15"], key_pairs(key)
+    rando = "02715"
+    assert_equal ["02", "27", "71", "15"], key_pairs(rando)
+  end
+
+  def test_offset_array
+    date = "040895"
+    assert_equal ["1", "0", "2", "5"], offset_array(date)
   end
 
   def test_sum_keys_and_offsets
-    key = key = "02715"
-    keys = key_pairs(key)
-    offsets = "040895"
-    assert_equal [3, 27, 73, 20], sum_keys_and_offsets(keys, offsets)
+      rando = "02715"
+      date = "040895"
+      keys = key_pairs(rando)
+      offsets = offset_array(date)
+      assert_equal [3, 27, 73, 20], sum_keys_and_offsets(keys, offsets)
   end
 end
