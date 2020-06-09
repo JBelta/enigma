3e311d7444f4655139130b0a985f9590e66fc34e
James Belta <james.belta@gmail.com>
2020-06-09 03:10:20 -0600

    Build encrypt runner

--- encrypt.rb
+++ encrypt.rb
@@ -2,12 +2,13 @@ require "./lib/enigma"
 
 enigma = Enigma.new
 
+#require 'pry'; binding.pry
 message = File.open(ARGV[0], "r")
-input = message.read
-encrypted = enigma.encrypt(input)
-code = encrypted[:encryption]
-require 'pry'; binding.pry
+input = message.read.chomp
+encrypted = enigma.encrypt(input, "02715", "040895")
 
 writer = File.open(ARGV[1], "w")
-writer.write(code)
+writer.write(encrypted[:encryption])
 writer.close
+
+puts "Created 'encrypted.txt' with the key #{encrypted[:key]} and the date #{encrypted[:date]}"
