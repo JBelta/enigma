095fc9a85d405217c4e512b3540004f3e58d4643
James Belta <james.belta@gmail.com>
2020-06-09 02:27:56 -0600

    Removed method

--- lib/offset.rb
+++ lib/offset.rb
@@ -7,9 +7,4 @@ class Offset
     @date = Time.now.strftime("%m%d%y")
     @numbers = (@date.to_i ** 2).to_s.split("").last(4)
   end
-
-  def last_four
-    four_num = @date.to_i ** 2
-    four_num.to_s.split("").last(4)
-  end
 end
